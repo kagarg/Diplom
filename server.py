cef0b238a39ffac714f2c9090b88b89212dd55be
kagarg <ivan.kurdukov@gmail.com>
2025-05-25 19:55:09 +0700

    1.0

--- server.py
+++ server.py
@@ -5,14 +5,14 @@ import os
 app = FastAPI()
 
 #  файлы существуют
-OVERLAY_FILE = "2.html"
+OVERLAY_FILE = "overlay.html"
 DATA_FILE = "battle_data.json"
 
-@app.get("/data")
-async def get_data():
-    if not os.path.exists(DATA_FILE):
-        return {"error": "Data file not found"}, 404
-    return FileResponse(DATA_FILE)
+# @app.get("/data")
+# async def get_data():
+#     if not os.path.exists(DATA_FILE):
+#         return {"error": "Data file not found"}, 404
+#     return FileResponse(DATA_FILE)
 
 @app.get("/overlay")
 async def get_overlay():
